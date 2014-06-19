14e0cfaa86b68699d6c45e2779be52532c6437b3
sojan v jose <sojan@pepalo.com>
2014-06-20 04:00:45 +0530

    initial commit for peep server

---
+++ readme.md
@@ -0,0 +1,3 @@
+check out the attached how to file to see how we can deploy the server to appengine 
+
+this is developed as a test server for peep app. 
---
+++ src/com/pepalo/messenger/server/Constants.java
@@ -0,0 +1,15 @@
+package com.pepalo.messenger.server;
+
+public class Constants {
+
+    /**
+     * Google API Console Browser key.
+     */
+	public static String API_KEY = "";//TODO provide API key e.g. AIzaSyGBu30MbYuf7U-CqBCanl09PfvulGe0E045
+
+	public static final String FROM = "chatId";
+	public static final String REG_ID = "regId";
+	public static final String MSG = "msg";
+	public static final String TO = "chatId2";
+	
+}
