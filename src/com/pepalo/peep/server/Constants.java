50da065b937429d6b0a78087fc49b85fed0ad58a
sojan v jose <sojan@pepalo.com>
2014-06-21 17:05:10 +0530

    bug fixes and changes

---
+++ src/com/pepalo/peep/server/Constants.java
@@ -0,0 +1,15 @@
+package com.pepalo.peep.server;
+
+public class Constants {
+
+    /**
+     * Google API Console Browser key.
+     */
+	public static String API_KEY = "AIzaSyANN_odoai6ZEENciXCQn_kHawfGrWem-A";//TODO provide API key e.g. AIzaSyGBu30MbYuf7U-CqBCanl09PfvulGe0E045
+
+	public static final String FROM = "chatId";
+	public static final String REG_ID = "regId";
+	public static final String MSG = "msg";
+	public static final String TO = "chatId2";
+	
+}
